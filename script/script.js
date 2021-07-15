942b145da79e66ca526a8e626673d59349768622
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-07-15 18:39:26 -0300

    mobile first

--- script/script.js
+++ script/script.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
-    $('.carousel').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-      });
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite:true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
+
+menuToggle()
+
+function menuToggle() {
+  let menuHamburguer = document.querySelector('#menuHamburguer')
+  let navHeader = document.querySelector('#navHeader')
+  let fecharMenu = document.querySelectorAll('#fecharMenu')
+
+  menuHamburguer.addEventListener('click', () => {
+    navHeader.classList.remove('menuHide')
+  })
+
+  fecharMenu.forEach((e) => {
+    e.addEventListener('click', () => { navHeader.classList.add('menuHide') })
+  })
+}
